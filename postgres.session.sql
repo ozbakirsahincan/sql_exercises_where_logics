f23dadc4993a7943d4a58b915e76c8386c73a551
ozbakirsahincan <ozbakirsahincan@gmail.com>
2024-07-10 12:53:02 +0200

    Lesson - 08 - Example

--- postgres.session.sql
+++ postgres.session.sql
@@ -13,3 +13,15 @@ where (rental_rate between 2 and 4) and (replacement_cost between 10 and 20) ord
 
 select length ,* from film where length in (40,50,60,70) order by length asc;
 select * from film where replacement_cost in (10.99,12.99,16.99)
+
+-- Lesson 08 Example
+
+-- film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+select * from film
+where replacement_cost between 12.99 and 16.98 order by replacement_cost desc;
+-- .actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+select first_name,last_name from actor where first_name in ('Penelope','Nick','Ed');
+
+-- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+select * from film
+where rental_rate  in (0.99,2.99,4.99) and replacement_cost in (12.99,15.99,28.99)
